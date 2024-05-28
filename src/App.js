fd6e5f532f0fc0f759a51d4965b3eda91fa6880f
GabrielaDias1 <gabrieladias539@gmail.com>
2024-05-28 03:12:30 -0300

    subindo projeto react conversor de moedas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+/*import React from "react";
+import ConversorDeMoedas from "./Components/ConversorDeMoedas";
 
-function App() {
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ConversorDeMoedas/>
     </div>
   );
 }
+export default App;*/
+import React from 'react'; 
 
-export default App;
+import ConversorDeMoedas from './Components/ConversorDeMoedas';
+
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <ConversorDeMoedas /> 
+
+    </div> 
+
+  ); 
+
+} 
+
+export default App; 
